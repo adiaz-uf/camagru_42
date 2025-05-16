58755e0abfb311cafff913790012a3d60990eef0
adiaz-uf <adiazufano13@gmail.com>
2025-05-16 10:52:29 +0200

    profile page, global styles

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Página cargada sin errores.");
 });
+
+const toggleButton = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+toggleButton.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
